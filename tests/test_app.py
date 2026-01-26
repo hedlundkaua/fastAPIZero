e77f16f4757d5044fb3021102f7f46a46bed86e6
hedlundkaua <hedlundkaua@gmail.com>
2026-01-26 13:07:37 -0300

    Implementando rotas CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -48,3 +48,28 @@ def test_get_lista_de_users(client):
             },
         ]
     }
+
+
+def test_update_user_to_list(client):
+
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
