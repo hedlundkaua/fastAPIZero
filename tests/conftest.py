ed7695b93672155d19bf0823f9dff41111dcb14f
hedlundkaua <hedlundkaua@gmail.com>
2026-01-29 12:52:30 -0300

    fix session and models User created

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,10 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from fast_zero.app import app
+from fast_zero.models import table_registry
 
 from fast_zero.app import app
 
@@ -7,3 +12,14 @@ from fast_zero.app import app
 @pytest.fixture
 def client():
     return TestClient(app)
+
+@pytest.fixture
+def session():
+    engine = create_engine('sqlite:///:memory:')
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+
+    table_registry.metadata.drop_all(engine)
+    engine.dispose()
