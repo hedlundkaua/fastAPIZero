b1801ef45e7df88c0a13aea685e7a4be6aa83d4f
hedlundkaua <hedlundkaua@gmail.com>
2026-02-03 20:21:26 -0300

    database criada e migrada e testes executados com sucesso mypc

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,20 +11,25 @@ from fast_zero.models import table_registry
 
 
 @contextmanager
-def _moc_db_time(*, model, time=datetime(2026, 1, 1)):
+def _moc_db_time(*, model, time=datetime(2026, 2, 1)):
+
     def fake_time_hook(mapper, connection, target):
         if hasattr(target, 'create_at'):
             target.created_at = time
-    
+        if hasattr(target, 'update_at'):
+            target.update_at = time
+
     event.listen(model, 'before_insert', fake_time_hook)
 
     yield time
 
     event.remove(model, 'before_insert', fake_time_hook)
 
+
 @pytest.fixture
 def mock_db_time():
     return _moc_db_time
+
 
 @pytest.fixture
 def client():
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,24 +1,25 @@
 from dataclasses import asdict
+
 from sqlalchemy import select
+
 from fast_zero.models import User
 
 
 def test_create_user(session, mock_db_time):
     with mock_db_time(model=User) as time:
-        new_user = User(
-            username='alice', password='secret', email='teste@test'
-        )
+        new_user = User(username='alice', password='secret', email='test@test')
         session.add(new_user)
         session.commit()
 
         user = session.scalar(select(User).where(User.username == 'alice'))
-        
+
         assert user.username == 'alice'
 
         assert asdict(user) == {
             'id': 1,
             'username': 'alice',
-            'password': '12345',
+            'password': 'secret',
             'email': 'test@test',
-            'created_at': time
+            'created_at': time,
+            'update_at': time,
         }
